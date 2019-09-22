9a58521b5eecebf1dfc148825f40bbfc10834abb
punctual-enhancer-1584 <amal.jasen@gmail.com>
2019-09-22 12:42:46 +0000

    Done.

--- lib/until.rb
+++ lib/until.rb
@@ -1,6 +1,11 @@
 def using_until
   levitation_force = 6
+  spell_name="Wingardium Leviosa"
   #your code here
+  until 10==levitation_force do
+    puts spell_name
+    levitation_force +=1
+  end
     
 end
 
